71c0090f26524766365bd8d41e2ec0c44cfd8189
DedBash <79027536+DedBash@users.noreply.github.com>
2023-01-17 13:34:50 +0100

    remove unnecessary
    Removed all v6 folders as they are redundant since the normal ones already have ipv6 support in them

--- Yandex DNS/YandexDNS4Magisk-family-ipv6/customize.sh
+++
@@ -1,5 +0,0 @@
-if [ -a /system/etc/resolv.conf ]; then
-	mkdir -p $MODPATH/system/etc/
-	printf "nameserver 77.88.8.7\nnameserver 77.88.8.3" >> $MODPATH/system/etc/resolv.conf
-	touch $MODPATH/auto_mount
-fi
--- Yandex DNS/YandexDNS4Magisk-ipv6/customize.sh
+++
@@ -1,5 +0,0 @@
-if [ -a /system/etc/resolv.conf ]; then
-	mkdir -p $MODPATH/system/etc/
-	printf "nameserver 77.88.8.8\nnameserver 77.88.8.1" >> $MODPATH/system/etc/resolv.conf
-	touch $MODPATH/auto_mount
-fi
